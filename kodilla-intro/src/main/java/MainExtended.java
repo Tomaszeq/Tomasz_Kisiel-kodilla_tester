274e884531051ed426b9d336369e046a77ef19a7
Tomasz Kisiel <hmhl7891@gmail.com>
2022-12-29 18:37:14 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended{
+    public static void main(String[] args) {
+        String markaSamochodu = "Alfa Romeo";
+        System.out.println(markaSamochodu);
+        String modelSamochodu = "Giulia";
+        System.out.println(modelSamochodu);
+        int rokProdukcji = 2022;
+        System.out.println(rokProdukcji);
+        double cenaSamochodu = 409.000;
+        System.out.println(cenaSamochodu);
+        double przyspieszenieOd0do100 = 5.2;
+        System.out.println(przyspieszenieOd0do100);
+        char klasaSamochodu = 'A';
+        System.out.println(klasaSamochodu);
+    }
+}
