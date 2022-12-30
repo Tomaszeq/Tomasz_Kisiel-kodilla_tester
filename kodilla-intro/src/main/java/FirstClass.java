7cc47e721ec66bb8b328647669143f91b5680e35
Tomasz Kisiel <hmhl7891@gmail.com>
2022-12-30 18:31:20 +0100

    Initial version

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -2,6 +2,12 @@ public class FirstClass {
     public static void main(String[] args) {         // [2]
         String example = ("Hello from FirstClass!");
         System.out.println(example);// [3]
+
+        if (1 > 2){
+            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
+        } else {
+            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
+        }
     }
 }
 
