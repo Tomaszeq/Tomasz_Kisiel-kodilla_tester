dd79ddffc019ffd3f17b913c7acadb1e6ad4c46c
Tomasz Kisiel <hmhl7891@gmail.com>
2022-12-30 17:29:02 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/HelloWorld2.java
@@ -0,0 +1,12 @@
+public class HelloWorld2 {
+    public static void main(String[] args0){
+        String example = "Hello World";
+        System.out.println(example);
+        String name = "Tomasz";
+        System.out.println(name);
+        int age = 35;
+        System.out.println(age);
+        char firstLetterFromLastName = 'K';
+        System.out.println(firstLetterFromLastName);
+    }
+}
