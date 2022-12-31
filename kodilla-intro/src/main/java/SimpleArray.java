09780d5841af81d4573b6b19338a8dc8bab43355
Tomasz Kisiel <hmhl7891@gmail.com>
2022-12-31 15:49:14 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,19 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String []names = new String [13];
+        names[0] = "Dwalin";
+        names[1] = "Balin";
+        names[2] = "Kili";
+        names[3] = "Fili";
+        names[4] = "Dori";
+        names[5] = "Nori";
+        names[6] = "Ori";
+        names[7] = "Oin";
+        names[8] = "Gloin";
+        names[9] = "Bifur";
+        names[10] = "Bofur";
+        names[11] = "Bombur";
+        names[12] = "Thorin III Debowa Tarcza";
+    }
+
+    }
