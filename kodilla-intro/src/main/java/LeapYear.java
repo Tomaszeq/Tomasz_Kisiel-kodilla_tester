8272e525e19a10f77347971c83de7a91565d14a8
Tomasz Kisiel <hmhl7891@gmail.com>
2022-12-31 15:18:34 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,14 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int rok = 2023;
+        System.out.println(rok);
+        if (rok % 4 == 0) {
+            System.out.println("Rok nie Przestepny");
+        } else if (rok % 100 == 0) {
+            System.out.println("Rok przestepny");
+        } else if (rok % 400 == 0) {
+            System.out.println("Rok Przestepny");
+        } else
+            System.out.println("Rok nie przestepny");
+    }
+}
