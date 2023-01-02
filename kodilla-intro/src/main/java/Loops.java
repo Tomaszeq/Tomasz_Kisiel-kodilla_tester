8e85dc08f7ae9468e0642b5bb4b9744e050a69be
Tomasz Kisiel <hmhl7891@gmail.com>
2023-01-02 16:25:30 +0100

    Initial version

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,9 +1,10 @@
 public class Loops {
     public static void main(String[] args) {
-        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i]);
+        int[] numbers = new int []{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++){
+            sum+=numbers[i];
+            System.out.println(sum);
         }
-
     }
 }
