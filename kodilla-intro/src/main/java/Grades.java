34ffae45657bdbc9ded3bf76089e9a266c0dca9a
Tomasz Kisiel <hmhl7891@gmail.com>
2023-01-18 16:24:28 +0100

    Initial version

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -6,6 +6,13 @@ public class Grades {
         this.grades = new int[10];
         this.size = 0;
 
+        int[] grades = {5, 5, 4, 4, 3, 3, 4, 3, 2, 3};
+        double sum = 0;
+        for (int i = 0; i < grades.length; i++){
+            sum += grades[i];
+        }
+        sum /= grades.length;
+        System.out.println("Średnia ocen: " + sum);
     }
     public void add(int value) {
         if (this.size == 10) {
@@ -13,5 +20,6 @@ public class Grades {
         }
         this.grades[this.size] = value;
         this.size++;
+        System.out.println("ostatnia ocena: " + value);
     }
 }
--- kodilla-intro/src/main/java/GradesApplication.java
+++ kodilla-intro/src/main/java/GradesApplication.java
@@ -1,17 +1,16 @@
 public class GradesApplication {
     public static void main(String[] args) {
-        Grades newGrade = new Grades();
+       Grades newGrade = new Grades();
 
-        newGrade.add(3);
-        newGrade.add(3);
-        newGrade.add(2);
         newGrade.add(5);
         newGrade.add(5);
-        newGrade.add(5);
-        newGrade.add(2);
+        newGrade.add(4);
         newGrade.add(4);
         newGrade.add(3);
         newGrade.add(3);
-
+        newGrade.add(4);
+        newGrade.add(3);
+        newGrade.add(2);
+        newGrade.add(3);
     }
 }
