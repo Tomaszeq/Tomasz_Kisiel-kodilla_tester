cd2908d44fcc9b9213b57dd939d1e769014b1b0d
Tomasz Kisiel <hmhl7891@gmail.com>
2023-01-26 15:22:46 +0100

    Initial version

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,15 +1,42 @@
 public class Application {
 
-    static class Person {
-        public String name;
-        public double age;
-        public double height;
+    private static String name;
+    private static double age;
+    private static double height;
 
-        public Person(String name, double age, double height) {
+    public Application (double age, double height) {
 
-            this.name = name;
-            this.age = age;
-            this.height = height;
+        this.name = name;
+        this.age = age;
+        this.height = height;
+
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static double getAge() {
+        return age;
+    }
+
+    public static double getHeight() {
+        return height;
+    }
+
+    public static void checkAge() {
+        if (age > 30) {
+            System.out.println("User is older than 30");
+        } else {
+            System.out.println("User is 30 (or younger)");
+        }
+    }
+
+    public static void checkHeight() {
+        if (height > 160) {
+            System.out.println("User is taller then 160");
+        } else {
+            System.out.println("user is 160 (or shorter");
         }
     }
 }
--- kodilla-intro/src/main/java/Person.java
+++ kodilla-intro/src/main/java/Person.java
@@ -1,12 +1,8 @@
 public class Person {
     public static void main(String[] args) {
 
-        Application.Person Adam = new Application.Person("Adam", 40.5, 178);
-
-        if (Adam.age > 30 && Adam.height > 160) {
-            System.out.println( "User is older than 30 and taller than 160cm");
-        } else {
-            System.out.println("User is 30 (or younger) or 160cm (or shorter)");
-        }
+        Application application = new Application (40.5, 178);
+        System.out.println(Application.getAge() + "" + Application.getHeight() + "");
+        Application.checkAge(); Application.checkHeight();
     }
 }
