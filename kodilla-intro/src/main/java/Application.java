cf4414aa947a99ffdc66f509caaab182ebe07f02
Tomasz Kisiel <hmhl7891@gmail.com>
2023-01-26 16:35:12 +0100

    Initial version

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,42 +1,8 @@
 public class Application {
+    public static void main(String[] args) {
 
-    private static String name;
-    private static double age;
-    private static double height;
-
-    public Application (double age, double height) {
-
-        this.name = name;
-        this.age = age;
-        this.height = height;
-
-    }
-
-    public static String getName() {
-        return name;
-    }
-
-    public static double getAge() {
-        return age;
-    }
-
-    public static double getHeight() {
-        return height;
-    }
-
-    public static void checkAge() {
-        if (age > 30) {
-            System.out.println("User is older than 30");
-        } else {
-            System.out.println("User is 30 (or younger)");
-        }
-    }
-
-    public static void checkHeight() {
-        if (height > 160) {
-            System.out.println("User is taller then 160");
-        } else {
-            System.out.println("user is 160 (or shorter");
-        }
+        Person person = new Person (40.5, 178);
+        System.out.println(Application.getAge() + "" + Application.getHeight() + "");
+        Application.checkAge(); Application.checkHeight();
     }
 }
--- kodilla-intro/src/main/java/Person.java
+++ kodilla-intro/src/main/java/Person.java
@@ -1,8 +1,43 @@
 public class Person {
-    public static void main(String[] args) {
+    private static String name;
+    private double age;
+    private double height;
 
-        Application application = new Application (40.5, 178);
-        System.out.println(Application.getAge() + "" + Application.getHeight() + "");
-        Application.checkAge(); Application.checkHeight();
+    public Person (double age, double height) {
+
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+
+
+
+    public void checkAge() {
+        if (this.age > 30) {
+            System.out.println("User is older than 30");
+        } else {
+            System.out.println("User is 30 (or younger)");
+        }
+    }
+
+    public void checkHeight() {
+        if (this.height > 160) {
+            System.out.println("User is taller then 160");
+        } else {
+            System.out.println("user is 160 (or shorter");
+        }
     }
 }
