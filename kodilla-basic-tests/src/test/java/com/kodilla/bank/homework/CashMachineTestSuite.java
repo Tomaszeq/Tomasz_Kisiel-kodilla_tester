25e4252d34e1e0e627db3aaa6718450ce2622c6b
Tomasz Kisiel <hmhl7891@gmail.com>
2023-02-19 12:52:14 +0100

    initial version

--- kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/CashMachineTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/CashMachineTestSuite.java
@@ -2,8 +2,71 @@ package com.kodilla.bank.homework;
 
 import org.junit.jupiter.api.Test;
 
+import javax.swing.plaf.PanelUI;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashMachineTestSuite {
+    @Test
+    public void testCashMachine() {
+        CashMachine cashMachine = new CashMachine();
+        int[] completedTransactions = cashMachine.getCompletedTransactions();
+        assertEquals(0, completedTransactions.length);
+    }
+    @Test
+    public void testCashMachine2() {
+        CashMachine cashMachine = new CashMachine();
+        cashMachine.add(1);
+        cashMachine.add(2);
+        cashMachine.add(3);
 
+        int[] completedTransactions = cashMachine.getCompletedTransactions();
+        assertEquals(3, completedTransactions.length);
+        assertEquals(1, completedTransactions[0]);
+        assertEquals(2, completedTransactions[1]);
+    }
+    @Test
+    public void testSaldo() {
+        CashMachine cashMachine = new CashMachine();
+        cashMachine.add(100);
+        cashMachine.add(150);
+        cashMachine.add(-150);
+        cashMachine.add(34);
+        cashMachine.add(0);
+        cashMachine.add(15);
+        assertEquals(99,99);
+
+    }
+    @Test
+    public void testWithdrawalQty() {
+        CashMachine cashMachine = new CashMachine();
+        cashMachine.add(100);
+        cashMachine.add(200);
+        cashMachine.add(400);
+        assertEquals(700,700);
+    }
+    @Test
+    public void testPaymentQty() {
+        CashMachine cashMachine = new CashMachine();
+        cashMachine.add(50);
+        cashMachine.add(100);
+        cashMachine.add(450);
+        assertEquals(600, 600);
+    }
+    @Test
+    public void testWithdrawalAvg() {
+        CashMachine cashMachine = new CashMachine();
+        cashMachine.add(100);
+        cashMachine.add(200);
+        cashMachine.add(400);
+        assertEquals(433.33, 433.33);
+    }
+    @Test
+    public void testPaymentAvg() {
+        CashMachine cashMachine = new CashMachine();
+        cashMachine.add(50);
+        cashMachine.add(100);
+        cashMachine.add(450);
+        assertEquals(300, 300);
+    }
 }
