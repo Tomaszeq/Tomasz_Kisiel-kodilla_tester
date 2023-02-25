19245a0f3e0809d328414f943be7a58c5502aa0d
Tomasz Kisiel <hmhl7891@gmail.com>
2023-02-25 16:01:08 +0100

    initial version

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/BookRecord.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.collections.adv.immutable;
-
-public record BookRecord(String author, String title, int year) {
-    public static void main(String[] args) {
-        BookRecord bookRecord = new BookRecord(“test”, “testowa pozycja”, 2022);
-    }
-}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskRecord.java
@@ -0,0 +1,7 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public record TaskRecord() {
+    public static void main(String[] args) {
+        Task task = new Task("Zadanie", 10);
+    }
+}
