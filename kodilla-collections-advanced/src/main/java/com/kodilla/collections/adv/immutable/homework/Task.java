26b2335bb88115ded6314034f9f6afb4f44b4e60
Tomasz Kisiel <hmhl7891@gmail.com>
2023-02-25 20:58:03 +0100

    initial version

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -2,10 +2,4 @@ package com.kodilla.collections.adv.immutable.homework;
 
 public record Task(String title, int duration) {
 
-    public String getTitle() {
-        return title;
-    }
-    public int getDuration() {
-        return duration;
-    }
 }
