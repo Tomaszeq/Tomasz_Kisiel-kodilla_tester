156f5a66fcd8c1d1328b0ebd10cda399e015449b
Tomasz Kisiel <hmhl7891@gmail.com>
2023-02-25 20:19:33 +0100

    initial version

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskRecord.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskRecord.java
@@ -1,5 +1,5 @@
 package com.kodilla.collections.adv.immutable.homework;
 
-public record TaskRecord(String title, int duration) {
-
+public record TaskRecord(String title, int duration, int year) {
+    static TaskRecord taskRecord = new TaskRecord("test", 22, 2222);
 }
