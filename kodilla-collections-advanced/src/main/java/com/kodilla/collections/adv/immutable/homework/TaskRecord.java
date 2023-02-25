936c7db135cc0b5b50b4f49555dbb26498123cdd
Tomasz Kisiel <hmhl7891@gmail.com>
2023-02-25 20:16:00 +0100

    initial version

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskRecord.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskRecord.java
@@ -1,7 +1,5 @@
 package com.kodilla.collections.adv.immutable.homework;
 
-public record TaskRecord() {
-    public static void main(String[] args) {
-        Task task = new Task("Zadanie", 10);
-    }
+public record TaskRecord(String title, int duration) {
+
 }
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskZmetodaMain.java
@@ -0,0 +1,7 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskZmetodaMain {
+    public static void main(String[] args) {
+        Task task = new Task("Zadanie", 10);
+    }
+}
