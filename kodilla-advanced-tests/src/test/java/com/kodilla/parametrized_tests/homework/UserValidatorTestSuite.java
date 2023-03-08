0f61d295232bcfddf59877ce36e6a5691958fcf5
Tomasz Kisiel <hmhl7891@gmail.com>
2023-03-08 14:33:34 +0100

    initial version

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -3,7 +3,6 @@ package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.SplittableRandom;
 
 import static org.junit.jupiter.api.Assertions.*;
 
