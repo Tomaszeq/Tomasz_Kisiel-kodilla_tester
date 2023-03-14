405c74005fa85bf5ce2f53490d014edd0b2c62cf
Tomasz Kisiel <hmhl7891@gmail.com>
2023-03-14 19:54:28 +0100

    initial version

--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Order.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Order.java
@@ -1,39 +1,4 @@
 package com.kodilla.execution_model.homework;
 
-import java.util.Date;
-
 public class Order {
-    private double value;
-    private Date date;
-    private String login;
-
-    public Order(double value, Date date, String login) {
-        this.value = value;
-        this.date = date;
-        this.login = login;
-    }
-
-    public double getValue() {
-        return value;
-    }
-
-    public void setValue(double value) {
-        this.value = value;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
 }
