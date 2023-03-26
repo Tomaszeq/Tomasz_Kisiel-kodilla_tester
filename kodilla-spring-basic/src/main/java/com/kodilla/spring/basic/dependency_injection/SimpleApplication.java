12cd0cd93bd1a3cd42d0cf5d4c0a33fb2bb0e4e8
Tomasz Kisiel <hmhl7891@gmail.com>
2023-03-26 18:20:03 +0200

    initial version

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/SimpleApplication.java
@@ -0,0 +1,24 @@
+package com.kodilla.spring.basic.dependency_injection;
+
+public class SimpleApplication {
+    private MessageService messageService = new MessageService() {
+        @Override
+        public void send(String message, String receiver) {
+
+        }
+    };
+
+    public SimpleApplication(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    public void processMessage(String message, String receiver) {
+        if (checkReceiver(receiver)) {
+            this.messageService.send(message, receiver);
+        }
+    }
+
+    private boolean checkReceiver(String receiver) {
+        return receiver != null && !receiver.isEmpty();
+    }
+}
