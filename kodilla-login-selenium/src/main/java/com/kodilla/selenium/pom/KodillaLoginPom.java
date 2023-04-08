1d7646b8def141bde409bc489787aeed5198e58c
Tomasz Kisiel <hmhl7891@gmail.com>
2023-04-08 09:55:32 +0200

    initial version

---
+++ kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/KodillaLoginPom.java
@@ -0,0 +1,43 @@
+package com.kodilla.selenium.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class KodillaLoginPom {
+
+    @FindBy(css = "input[type='email']")
+    WebElement emailField;
+
+    @FindBy(css = "input[type='password']")
+    WebElement passwordField;
+
+    @FindBy(xpath = "//button[contains(text(), 'Log in')]")
+    WebElement loginButton;
+
+    WebDriver driver;
+
+    public KodillaLoginPom() {
+        System.setProperty("webdriver.chrome.driver", "/Users/tomaszkisiel/Documents/Kodilla-course/kodilla-course/chromedriver-kopia");
+        driver = new ChromeDriver();
+        PageFactory.initElements(driver, this);
+
+        driver.navigate().to("https://kodilla.com/pl/test/login");
+
+    }
+
+    public boolean login(String email, String password) {
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+        loginButton.click();
+        String message = driver.switchTo().alert().getText();
+        driver.switchTo().alert().dismiss();
+        return message.equals("Jesteś teraz zalogowany!");
+    }
+
+    public void close() {
+        driver.close();
+    }
+}
