e6f5c5fe1f7b77b50f432d69db23a17373e3123c
Tomasz Kisiel <hmhl7891@gmail.com>
2023-04-18 10:56:48 +0200

    initial version

--- kodilla-code-coverage/src/main/java/com/kodilla/jacoco/Year.java
+++ kodilla-code-coverage/src/main/java/com/kodilla/jacoco/Year.java
@@ -1,18 +1,18 @@
 package com.kodilla.jacoco;
 
 public class Year {
-    private final int year;
+    private final int yearValue;
 
-    public Year(int year) {
-        this.year = year;
+    public Year(int yearValue) {
+        this.yearValue = yearValue;
     }
 
     public boolean isLeap() {
-        if (this.year % 4 != 0) {
+        if (this.yearValue % 4 != 0) {
             return false;
-        } else if (this.year % 100 != 0) {
+        } else if (this.yearValue % 100 != 0) {
             return true;
-        } else if (this.year % 400 != 0) {
+        } else if (this.yearValue % 400 != 0) {
             return false;
         } else {
             return true;
